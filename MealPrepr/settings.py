e1e22b5ec29652e3485c917331982174d61a7094
Nagatyy <mohamednagaty@aucegypt.edu>
2021-01-27 01:08:22 +0200

    Add heroku-django

--- MealPrepr/settings.py
+++ MealPrepr/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -184,7 +184,7 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = 'hello@mealmill.co'
 
-
+django_heroku.settings(locals())
 
 
 
