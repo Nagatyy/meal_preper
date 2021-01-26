aa542a11f7ed4f93d6cc032ab7d95382b6e28750
Nagatyy <mohamednagaty@aucegypt.edu>
2021-01-26 23:49:17 +0200

    Bug Fixes

--- MealPrepr/settings.py
+++ MealPrepr/settings.py
@@ -173,8 +173,8 @@ EMAIL_PAGE_DOMAIN = 'https://mealmill.co/'
 # EMAIL_PAGE_DOMAIN = 'http://127.0.0.1:8000/'
 
 # For Django Email Backend
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtppro.zoho.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hello@mealmill.co'
