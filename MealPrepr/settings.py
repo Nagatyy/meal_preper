cb36cb72a938ba6d98db2076e0caf2195b337883
Nagatyy <mohamednagaty@aucegypt.edu>
2021-01-27 01:19:34 +0200

    Set debug value

--- MealPrepr/settings.py
+++ MealPrepr/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('MEAL_MILL_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 # ALLOWED_HOSTS = ['192.168.1.19', 'localhost', '127.0.0.1']
 ALLOWED_HOSTS = ['*']
