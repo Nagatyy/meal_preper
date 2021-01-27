1f8523f75eba6369a9097f2687e9b44b17125a67
Nagatyy <mohamednagaty@aucegypt.edu>
2021-01-27 21:01:13 +0200

    Set email backend

--- MealPrepr/settings.py
+++ MealPrepr/settings.py
@@ -174,8 +174,8 @@ EMAIL_PAGE_DOMAIN = 'https://www.mealmill.co/'
 # EMAIL_PAGE_DOMAIN = 'http://127.0.0.1:8000/'
 
 # For Django Email Backend
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtppro.zoho.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hello@mealmill.co'
